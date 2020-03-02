d2540e19f9126aa0afa08cd17469ae696c637409
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-03-02 21:48:54 +0100

    lesson one completed, simple http server in go using standard library

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("hello world")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("goodbye world")
+	})
+	http.ListenAndServe(":9090", nil)
 }
