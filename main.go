f74271bb5adcb17704d4481bda578fcb1f16456b
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-03-02 22:19:22 +0100

    added graceful shutdown and packaged the http better

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/vsivarajah/Go-rest/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("hello world")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("goodbye world")
-	})
-	http.ListenAndServe(":9090", nil)
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
