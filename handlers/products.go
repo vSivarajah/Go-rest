7473ee585752c22ca9e1ed869e498da8fe164ce0
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-03-19 11:31:48 +0100

    added add products

--- handlers/products.go
+++ handlers/products.go
@@ -20,14 +20,24 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProducts(rw, r)
+	}
+
 	//Catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET products")
+
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST products")
 }
